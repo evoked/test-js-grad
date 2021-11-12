4671e24051755661d59a6697561b3ef74fd82800
evoked <3vok3d@gmail.com>
2021-11-12 15:43:17 +0000

    add organise maintainer function

--- src/03-organise-maintainers/index.js
+++ src/03-organise-maintainers/index.js
@@ -35,12 +35,57 @@ The results should have this structure:
     }
     ...
 ]
- * NOTE: the parent array and each "packageNames" array should 
- * be in alphabetical order.
- */
-
+* NOTE: the parent array and each "packageNames" array should 
+* be in alphabetical order.
+*/
+const axios = require('axios')
 module.exports = async function organiseMaintainers() {
-  // TODO
+	const { data } = await axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
+		"url": "https://api.npms.io/v2/search/suggestions?q=react",
+		"method": "GET",
+		"return_payload": true
+	})
 
-  return maintainers
+	let contents = []
+
+	data.content.forEach(element => {
+		contents.push(element)
+	});
+
+	// Initialise two seperate arrays to allow us to create a sorted array of objects later, then 
+	// store package names into the index of the respective user.
+	let maintainerUsername = []
+	let packageMaintainers = []
+
+	contents.forEach(content => {
+		const usernames = []
+		for(const user of content.package.maintainers) {
+			if(!maintainerUsername.includes(user.username)) {
+				maintainerUsername.push(user.username)
+			}
+			usernames.push(user.username)
+		}
+		packageMaintainers.push({name: content.package.name, usernames: usernames})
+	})
+
+	maintainerUsername.sort()
+
+	let maintainers = []
+
+	for(const username of maintainerUsername) {
+		maintainers.push({username: username, packageNames: []})
+	}
+
+	for(const element of packageMaintainers) {
+		for (const user of element.usernames) {
+			const index = maintainerUsername.indexOf(user)
+			if (index === -1) {
+				continue
+			}
+			maintainers[index].packageNames.push(element.name)
+			maintainers[index].packageNames.sort()
+		}
+	}
+	
+	return maintainers
 };
