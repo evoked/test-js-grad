2c2aa15201cb954658b45433dc0eb1a8e1d7df6f
evoked <3vok3d@gmail.com>
2021-11-12 15:56:51 +0000

    update comments for flow of function

--- src/03-organise-maintainers/index.js
+++ src/03-organise-maintainers/index.js
@@ -57,33 +57,46 @@ module.exports = async function organiseMaintainers() {
 	let maintainerUsername = []
 	let packageMaintainers = []
 
+	// We're going to iterate through every indice in contents, and add usernames to the 
 	contents.forEach(content => {
 		const usernames = []
 		for(const user of content.package.maintainers) {
+			// Check if username has already been added to overall user array
 			if(!maintainerUsername.includes(user.username)) {
 				maintainerUsername.push(user.username)
 			}
+			// Always push every username into usernames array
 			usernames.push(user.username)
 		}
+		// Finally, add object to packageMaintainers containing package name & all usernames in package
 		packageMaintainers.push({name: content.package.name, usernames: usernames})
 	})
 
+	// Sort usernames in ascending order using the array sort function
 	maintainerUsername.sort()
 
 	let maintainers = []
 
+	// Start creating maintainers array by pushing sorted list of users into seperate objects 
+	// & respectively adding a packageName array 
 	for(const username of maintainerUsername) {
 		maintainers.push({username: username, packageNames: []})
 	}
 
+	// Iterate through every object within array consisting of package name and respective users
 	for(const element of packageMaintainers) {
+		// Iterate through every user in usernames array
 		for (const user of element.usernames) {
+			// Find the index of the sorted username using the sorted array
 			const index = maintainerUsername.indexOf(user)
 			if (index === -1) {
 				continue
 			}
+			// Finally, add everything to the maintainers array & iteratively sort them if they have more than 1 contribution
 			maintainers[index].packageNames.push(element.name)
-			maintainers[index].packageNames.sort()
+			if(maintainers[index].packageNames.length > 1) {
+				maintainers[index].packageNames.sort()
+			}
 		}
 	}
 	
